2c56dda22b26be2d02d6289450d04440ddf0fda5
bluelobster06-oss <bluelobster06@gmail.com>
2026-08-14 02:52:33 +0200

    Updated custom pokemon data and local setup

--- js/router.js
+++ js/router.js
@@ -1,37 +1,41 @@
 var Pokedex = Panels.App.extend({
-	topbarView: Topbar,
-	backButtonPrefix: '<i class="fa fa-chevron-left"></i> ',
-	states2: {
-		'pokemon/:pokemon': PokedexPokemonPanel,
-		'moves/:move': PokedexMovePanel,
-		'items/:item': PokedexItemPanel,
-		'abilities/:ability': PokedexAbilityPanel,
-		'types/:type': PokedexTypePanel,
-		'categories/:category': PokedexCategoryPanel,
-		'tags/:tag': PokedexTagPanel,
-		'egggroups/:egggroup': PokedexEggGroupPanel,
-		'tiers/:tier': PokedexTierPanel,
-		'articles/:article': PokedexArticlePanel,
+    topbarView: Topbar,
+    backButtonPrefix: '<i class="fa fa-chevron-left"></i> ',
+    states2: {
+        'pokemon/:pokemon': PokedexPokemonPanel,
+        'moves/:move': PokedexMovePanel,
+        'items/:item': PokedexItemPanel,
+        'abilities/:ability': PokedexAbilityPanel,
+        'types/:type': PokedexTypePanel,
+        'categories/:category': PokedexCategoryPanel,
+        'tags/:tag': PokedexTagPanel,
+        'egggroups/:egggroup': PokedexEggGroupPanel,
+        'tiers/:tier': PokedexTierPanel,
+        'articles/:article': PokedexArticlePanel,
 
-		'': PokedexSearchPanel,
-		'pokemon/': PokedexSearchPanel,
-		'moves/': PokedexSearchPanel,
-		':q': PokedexSearchPanel
-	},
-	initialize: function() {
-		this.routePanel('*path', PokedexSearchPanel); // catch-all default
+        '': PokedexSearchPanel,
+        'pokemon/': PokedexSearchPanel,
+        'moves/': PokedexSearchPanel,
+        ':q': PokedexSearchPanel
+    },
+    initialize: function () {
+        this.routePanel('*path', PokedexSearchPanel); // catch-all default
 
-		for (var i in this.states2) {
-			this.routePanel(i, this.states2[i]);
-		}
-	}
+        for (var i in this.states2) {
+            this.routePanel(i, this.states2[i]);
+        }
+    }
 });
 
 var rootPath = location.pathname;
 if (!rootPath.endsWith('/')) {
-	rootPath = rootPath.substring(0, rootPath.lastIndexOf('/') + 1);
+    rootPath = rootPath.substring(0, rootPath.lastIndexOf('/') + 1);
 }
 if (typeof BattleSearch !== 'undefined') {
-	BattleSearch.urlRoot = rootPath;
+    BattleSearch.urlRoot = rootPath;
 }
 var pokedex = new Pokedex({ root: rootPath });
+
+if (window.location.pathname === "/" || window.location.pathname === "/Pokemon-Showdown-Dex/" || window.location.pathname === "/Pokemon-Showdown-Dex") {
+    window.location.replace(window.location.origin + "/Pokemon-Showdown-Dex/#/");
+}
