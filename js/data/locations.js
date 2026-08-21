85e0de143e2266b8656c9a0c90c2b901806fc7ea
bluelobster06-oss <bluelobster06@gmail.com>
2026-08-21 23:31:54 +0200

    Improved encounter sorting by time of day, added Route 1 + Cindering Grove

--- js/data/locations.js
+++ js/data/locations.js
@@ -8,8 +8,8 @@ var PokedexLocationIcons = {
         'Rock Smash': '/images/RockSmash.png'
     },
     time: {
-        'Nighttime Only': '/images/MoonRR.png',
-        'Daytime Only': '/images/SunRR.png'
+        'Nighttime': '/images/MoonRR2.png',
+        'Daytime': '/images/SunRR2.jpg'
     }
 };
 
@@ -20,13 +20,13 @@ var PokedexLocations = {
             { pokemon: 'bunnelby', name: 'Bunnelby', rate: 20, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
             { pokemon: 'dwebble', name: 'Dwebble', rate: 15, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
             { pokemon: 'chewtle', name: 'Chewtle', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
-            { pokemon: 'cubone', name: 'Cubone', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'cubone', name: 'Cubone', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'], heldItem: { name: 'Thick Club', rate: 5 } },
             { pokemon: 'cufant', name: 'Cufant', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
             { pokemon: 'pawniard', name: 'Pawniard', rate: 10, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
             { pokemon: 'baltoy', name: 'Baltoy', rate: 7, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
             { pokemon: 'skorupi', name: 'Skorupi', rate: 7, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
             { pokemon: 'swinub', name: 'Swinub', rate: 6, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
-            { pokemon: 'diglett', name: 'Diglett', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
+            { pokemon: 'diglett', name: 'Diglett', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass'], heldItem: { name: 'Soft Sand', rate: 100 } },
 
         ]
     },
@@ -35,12 +35,41 @@ var PokedexLocations = {
         encounters: [
             { pokemon: 'lotad', name: 'Lotad', rate: 20, minLevel: 2, maxLevel: 5, tags: ['Old Rod'] },
             { pokemon: 'poliwag', name: 'Poliwag', rate: 20, minLevel: 2, maxLevel: 5, tags: ['Old Rod'] },
-            { pokemon: 'ducklett', name: 'Ducklett', rate: 15, minLevel: 2, maxLevel: 5, tags: ['Old Rod', 'Daytime Only'] },
+            { pokemon: 'ducklett', name: 'Ducklett', rate: 15, minLevel: 2, maxLevel: 5, tags: ['Old Rod', 'Daytime'] },
             { pokemon: 'psyduck', name: 'Psyduck', rate: 15, minLevel: 2, maxLevel: 5, tags: ['Old Rod'] },
-            { pokemon: 'surskit', name: 'Surskit', rate: 10, minLevel: 2, maxLevel: 5, tags: ['Old Rod', 'Nighttime Only'] },
+            { pokemon: 'tympole', name: 'Tympole', rate: 15, minLevel: 2, maxLevel: 5, tags: ['Old Rod'] },
+            { pokemon: 'surskit', name: 'Surskit', rate: 10, minLevel: 2, maxLevel: 5, tags: ['Old Rod', 'Nighttime'] },
             { pokemon: 'goldeen', name: 'Goldeen', rate: 5, minLevel: 2, maxLevel: 5, tags: ['Old Rod'] },
             { pokemon: 'gyarados', name: 'Gyarados', rate: 5, minLevel: 25, maxLevel: 30, tags: ['Good Rod'] },
         ]
     },
-
+    route1: {
+        name: 'Route 1',
+        encounters: [
+            { pokemon: 'fletchling', name: 'Fletchling', rate: 15, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
+            { pokemon: 'pidove', name: 'Pidove', rate: 12, minLevel: 3, maxLevel: 4, tags: ['Grass', 'Daytime'] },
+            { pokemon: 'pidgey', name: 'Pidgey', rate: 12, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'starly', name: 'Starly', rate: 12, minLevel: 3, maxLevel: 4, tags: ['Grass', 'Nighttime'] },
+            { pokemon: 'wurmple', name: 'Wurmple', rate: 8, minLevel: 2, maxLevel: 3, tags: ['Grass'], heldItem: { name: 'Pecha Berry', rate: 50 } },
+            { pokemon: 'wooloo', name: 'Wooloo', rate: 8, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
+            { pokemon: 'taillow', name: 'Taillow', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass', 'Daytime'] },
+            { pokemon: 'skwovet', name: 'Skwovet', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass', 'Nighttime'], heldItem: { name: 'Oran Berry', rate: 25 } },
+            { pokemon: 'sentret', name: 'Sentret', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass', 'Daytime'] },
+            { pokemon: 'lechonk', name: 'Lechonk', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass'] },
+            { pokemon: 'rookidee', name: 'Rookidee', rate: 5, minLevel: 2, maxLevel: 3, tags: ['Grass', 'Daytime'] },
+        ]
+    },
+    cinderinggrove: {
+        name: 'Cindering Grove',
+        encounters: [
+            { pokemon: 'magby', name: 'Magby', rate: 20, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'darumaka', name: 'Darumaka', rate: 20, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'numel', name: 'Numel', rate: 15, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'salandit', name: 'Salandit', rate: 15, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'ponyta', name: 'Ponyta', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'litleo', name: 'Litleo', rate: 10, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'growlithehisui', name: 'Growtlithe', rate: 5, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+            { pokemon: 'larvesta', name: 'Larvesta', rate: 5, minLevel: 3, maxLevel: 4, tags: ['Grass'] },
+        ]
+    },
 };
--- js/pokedex-locations.js
+++ js/pokedex-locations.js
@@ -1,5 +1,5 @@
-var LOCATION_TABLES = ['All', 'Grass', 'Gift', 'Old Rod', 'Good Rod', 'Surf', 'Rock Smash'];
-var LOCATION_TIMES = ['Any time', 'Nighttime Only', 'Daytime Only'];
+var LOCATION_TABLES = ['Grass', 'Gift', 'Old Rod', 'Good Rod', 'Surf', 'Rock Smash'];
+var LOCATION_TIMES = ['Any time', 'Daytime', 'Nighttime'];
 
 var PokedexLocationsPanel = Panels.Panel.extend({
     minWidth: 639,
@@ -38,7 +38,6 @@ var PokedexLocationPanel = PokedexResultPanel.extend({
         this.locationID = toID(locationID);
         this.location = PokedexLocations[this.locationID];
         this.sortDirection = 'desc';
-        this.tableIndex = -1;
         this.timeIndex = -1;
         this.shortTitle = this.location ? this.location.name : 'Locations';
         this.render();
@@ -50,10 +49,6 @@ var PokedexLocationPanel = PokedexResultPanel.extend({
     cycleOption: function (e) {
         var option = $(e.currentTarget).data('option');
         if (option === 'rate') this.sortDirection = this.sortDirection === 'desc' ? 'asc' : 'desc';
-        if (option === 'table') {
-            var tables = this.getAvailableTables();
-            if (tables.length) this.tableIndex = this.tableIndex >= tables.length - 1 ? -1 : this.tableIndex + 1;
-        }
         if (option === 'time') {
             var times = this.getAvailableTimes();
             if (times.length) this.timeIndex = this.timeIndex >= times.length - 1 ? -1 : this.timeIndex + 1;
@@ -72,63 +67,128 @@ var PokedexLocationPanel = PokedexResultPanel.extend({
         buf += '</div>';
         this.html(buf);
     },
+    isEncounterAvailableAtTime: function (encounter, time) {
+        if (!time || time === 'Any time') return true;
+        var tags = encounter.tags || [];
+        var isNightOnly = tags.indexOf('Nighttime Only') >= 0 || tags.indexOf('Nighttime') >= 0;
+        var isDayOnly = tags.indexOf('Daytime Only') >= 0 || tags.indexOf('Daytime') >= 0;
+        if (time === 'Daytime') return !isNightOnly;
+        if (time === 'Nighttime') return !isDayOnly;
+        return true;
+    },
     renderEncounters: function () {
-        var encounters = this.location.encounters.slice();
-        var table = this.getSelectedTable();
-        var time = this.getSelectedTime();
-        encounters = encounters.filter(function (encounter) {
-            if (table !== 'All' && encounter.tags.indexOf(table) < 0) return false;
-            if (time !== 'Any time' && encounter.tags.indexOf(time) < 0) return false;
-            return true;
-        });
-        encounters.sort(function (a, b) {
-            return this.sortDirection === 'asc' ? a.rate - b.rate : b.rate - a.rate;
-        }.bind(this));
+        var allEncounters = this.location.encounters.slice();
+        var selectedTime = this.getSelectedTime();
 
         var buf = '<li class="resultheader location-header"><h3>Encounters</h3><strong class="location-sort-label">Sort:</strong>';
         buf += '<button class="location-cycle rate-cycle" data-option="rate">Rate %<small>' + this.getRateLabel() + '</small></button>';
-        buf += '<button class="location-cycle table-cycle" data-option="table">Table<small>' + Dex.escapeHTML(table) + '</small></button>';
-        buf += '<button class="location-cycle time-cycle" data-option="time">Time of Day<small>' + Dex.escapeHTML(time) + '</small></button>';
+        buf += '<button class="location-cycle time-cycle" data-option="time">Time of Day<small>' + Dex.escapeHTML(selectedTime) + '</small></button>';
         buf += '<span class="location-level-label">Level</span></li>';
-        if (!encounters.length) return buf + '<li class="notfound"><em>No encounters match these filters.</em></li>';
-        for (var i = 0; i < encounters.length; i++) {
-            var encounter = encounters[i];
-            var encounterTable = this.getEncounterTable(encounter);
-            var encounterTime = this.getEncounterTime(encounter);
-            var level = encounter.minLevel === encounter.maxLevel ? 'Lv. ' + encounter.minLevel : 'Lv. ' + encounter.minLevel + '&ndash;' + encounter.maxLevel;
-            buf += '<li class="location-encounter"><div class="encounter-pokemon">' + this.renderPokemonButton(encounter) + '</div><span class="encounter-sort-spacer"></span><span class="encounter-rate">' + encounter.rate + '%</span><span class="encounter-table">' + this.renderMarker('table', encounterTable) + encounterTable + '</span><span class="encounter-time">' + this.renderMarker('time', encounterTime) + encounterTime + '</span><span class="encounter-level">' + level + '</span></li>';
+
+        var tableOrder = [];
+        for (var t = 0; t < LOCATION_TABLES.length; t++) {
+            var tabName = LOCATION_TABLES[t];
+            var hasTab = allEncounters.some(function (enc) {
+                return enc.tags && enc.tags.indexOf(tabName) >= 0;
+            });
+            if (hasTab && tableOrder.indexOf(tabName) < 0) tableOrder.push(tabName);
         }
+        for (var e = 0; e < allEncounters.length; e++) {
+            var encTable = this.getEncounterTable(allEncounters[e]);
+            if (encTable && encTable !== '&ndash;' && tableOrder.indexOf(encTable) < 0) {
+                tableOrder.push(encTable);
+            }
+        }
+        if (!tableOrder.length) tableOrder = ['Encounters'];
+
+        var isAsc = this.sortDirection === 'asc';
+        var totalRendered = 0;
+
+        for (var k = 0; k < tableOrder.length; k++) {
+            var currentTable = tableOrder[k];
+            var allTableEncounters = allEncounters.filter(function (encounter) {
+                if (currentTable === 'Encounters') return true;
+                return encounter.tags && encounter.tags.indexOf(currentTable) >= 0;
+            });
+            if (!allTableEncounters.length) continue;
+
+            var activeTableEncounters = allTableEncounters.filter(function (encounter) {
+                return this.isEncounterAvailableAtTime(encounter, selectedTime);
+            }.bind(this));
+            if (!activeTableEncounters.length) continue;
+
+            var baseTableTotal = allTableEncounters.reduce(function (sum, enc) {
+                return sum + (enc.rate || 0);
+            }, 0);
+            var unavailableSum = allTableEncounters.filter(function (encounter) {
+                return !this.isEncounterAvailableAtTime(encounter, selectedTime);
+            }.bind(this)).reduce(function (sum, enc) {
+                return sum + (enc.rate || 0);
+            }, 0);
+
+            var multiplier = 1;
+            if (selectedTime !== 'Any time' && unavailableSum > 0) {
+                var assumedPool = baseTableTotal >= 80 && baseTableTotal <= 120 ? 100 : baseTableTotal;
+                var activePool = assumedPool - unavailableSum;
+                if (activePool > 0) {
+                    multiplier = assumedPool / activePool;
+                }
+            }
+
+            var displayEncounters = activeTableEncounters.map(function (encounter) {
+                var displayRate = encounter.rate;
+                if (selectedTime !== 'Any time' && multiplier !== 1) {
+                    displayRate = Math.round(encounter.rate * multiplier * 2) / 2;
+                }
+                return {
+                    encounter: encounter,
+                    displayRate: displayRate
+                };
+            });
+
+            displayEncounters.sort(function (a, b) {
+                return isAsc ? a.displayRate - b.displayRate : b.displayRate - a.displayRate;
+            });
+
+            if (currentTable !== 'Encounters') {
+                buf += '<li class="resultheader location-table-header"><h3>' + this.renderMarker('table', currentTable) + Dex.escapeHTML(currentTable) + '</h3></li>';
+            }
+
+            for (var i = 0; i < displayEncounters.length; i++) {
+                totalRendered++;
+                var item = displayEncounters[i];
+                var encounter = item.encounter;
+                var encounterTime = this.getEncounterTime(encounter);
+                var level = encounter.minLevel === encounter.maxLevel ? 'Lv. ' + encounter.minLevel : 'Lv. ' + encounter.minLevel + '&ndash;' + encounter.maxLevel;
+                var rateStr = (item.displayRate % 1 === 0 ? item.displayRate : item.displayRate.toFixed(1)) + '%';
+
+                buf += '<li class="location-encounter">' +
+                    '<div class="encounter-pokemon">' + this.renderPokemonButton(encounter) + '</div>' +
+                    '<span class="encounter-sort-spacer"></span>' +
+                    '<span class="encounter-rate">' + rateStr + '</span>' +
+                    '<span class="encounter-time">' + this.renderMarker('time', encounterTime) + encounterTime + '</span>' +
+                    '<span class="encounter-level">' + level + '</span>' +
+                    '</li>';
+            }
+        }
+
+        if (!totalRendered) return buf + '<li class="notfound"><em>No encounters match these filters.</em></li>';
         return buf;
     },
-    getAvailableTables: function () {
-        var tables = [];
-        for (var i = 1; i < LOCATION_TABLES.length; i++) {
-            var table = LOCATION_TABLES[i];
-            for (var j = 0; j < this.location.encounters.length; j++) {
-                if (this.location.encounters[j].tags.indexOf(table) >= 0) {
-                    tables.push(table);
-                    break;
-                }
-            }
-        }
-        return tables;
-    },
     getAvailableTimes: function () {
-        var times = [];
-        for (var i = 1; i < LOCATION_TIMES.length; i++) {
-            var time = LOCATION_TIMES[i];
-            for (var j = 0; j < this.location.encounters.length; j++) {
-                if (this.location.encounters[j].tags.indexOf(time) >= 0) {
-                    times.push(time);
-                    break;
-                }
-            }
+        var hasDay = false;
+        var hasNight = false;
+        for (var j = 0; j < this.location.encounters.length; j++) {
+            var tags = this.location.encounters[j].tags || [];
+            if (tags.indexOf('Daytime Only') >= 0 || tags.indexOf('Daytime') >= 0) hasDay = true;
+            if (tags.indexOf('Nighttime Only') >= 0 || tags.indexOf('Nighttime') >= 0) hasNight = true;
+        }
+        var times = ['Any time'];
+        if (hasDay || hasNight) {
+            times.push('Daytime');
+            times.push('Nighttime');
         }
         return times;
-    },
-    getSelectedTable: function () {
-        var tables = this.getAvailableTables();
-        return this.tableIndex >= 0 && tables[this.tableIndex] ? tables[this.tableIndex] : 'All';
     },
     getSelectedTime: function () {
         var times = this.getAvailableTimes();
@@ -138,14 +198,15 @@ var PokedexLocationPanel = PokedexResultPanel.extend({
         return this.sortDirection === 'desc' ? 'Highest' : 'Lowest';
     },
     getEncounterTable: function (encounter) {
-        for (var i = 1; i < LOCATION_TABLES.length; i++) {
-            if (encounter.tags.indexOf(LOCATION_TABLES[i]) >= 0) return LOCATION_TABLES[i];
+        for (var i = 0; i < LOCATION_TABLES.length; i++) {
+            if (encounter.tags && encounter.tags.indexOf(LOCATION_TABLES[i]) >= 0) return LOCATION_TABLES[i];
         }
         return '&ndash;';
     },
     getEncounterTime: function (encounter) {
-        if (encounter.tags.indexOf('Nighttime Only') >= 0) return 'Nighttime Only';
-        if (encounter.tags.indexOf('Daytime Only') >= 0) return 'Daytime Only';
+        var tags = encounter.tags || [];
+        if (tags.indexOf('Nighttime Only') >= 0 || tags.indexOf('Nighttime') >= 0) return 'Nighttime';
+        if (tags.indexOf('Daytime Only') >= 0 || tags.indexOf('Daytime') >= 0) return 'Daytime';
         return '&ndash;';
     },
     renderMarker: function (group, name) {
@@ -153,9 +214,56 @@ var PokedexLocationPanel = PokedexResultPanel.extend({
         if (!icon) return '';
         return '<img class="location-marker-icon" src="' + Dex.escapeHTML(icon) + '" alt="" /> ';
     },
+    renderHeldItemBadges: function (encounter) {
+        var rawItems = encounter.heldItems || (encounter.heldItem ? [encounter.heldItem] : []);
+        if (!rawItems.length && encounter.item) {
+            rawItems = [{
+                name: encounter.item,
+                rate: encounter.itemRate != null ? encounter.itemRate : 100,
+                icon: encounter.itemIcon || ''
+            }];
+        }
+        if (!rawItems.length) return '';
+
+        var badges = [];
+        for (var i = 0; i < rawItems.length; i++) {
+            var entry = rawItems[i];
+            if (typeof entry === 'string') {
+                entry = { name: entry, rate: encounter.itemRate != null ? encounter.itemRate : 100 };
+            }
+            var itemName = entry.name || entry.item || '';
+            if (!itemName) continue;
+            var rate = entry.rate != null ? entry.rate : (encounter.itemRate != null ? encounter.itemRate : 100);
+            var titleText = rate + '% Chance To Hold ' + itemName + '';
+            var iconUrl = entry.icon || entry.image || entry.src || '';
+
+            var iconHtml = '';
+            if (iconUrl) {
+                iconHtml = '<img class="encounter-held-item-icon" src="' + Dex.escapeHTML(iconUrl) + '" alt="' + Dex.escapeHTML(itemName) + '" />';
+            } else {
+                var itemID = toID(itemName);
+                var itemData = (typeof BattleItems !== 'undefined' && BattleItems[itemID]) || (typeof Dex !== 'undefined' && Dex.items && Dex.items.get(itemID));
+                if (itemData && typeof Dex !== 'undefined' && Dex.getItemIcon) {
+                    iconHtml = '<span class="itemicon encounter-held-item-sprite" style="' + Dex.getItemIcon(itemData) + '"></span>';
+                } else {
+                    iconHtml = '<span class="encounter-held-item-fallback">' + Dex.escapeHTML(itemName.charAt(0)) + '</span>';
+                }
+            }
+
+            badges.push('<span class="encounter-held-item-badge" title="' + Dex.escapeHTML(titleText) + '" aria-label="' + Dex.escapeHTML(titleText) + '">' + iconHtml + '</span>');
+        }
+
+        if (!badges.length) return '';
+        return '<span class="encounter-held-items">' + badges.join('') + '</span>';
+    },
     renderPokemonButton: function (encounter) {
         var pokemon = typeof BattlePokedex !== 'undefined' && BattlePokedex[encounter.pokemon];
         var name = encounter.name || (pokemon && pokemon.name) || encounter.pokemon;
-        return '<a class="location-pokemon-button" href="/pokemon/' + encounter.pokemon + '" data-target="push"><span class="picon" style="' + Dex.getPokemonIcon(encounter.pokemon) + '"></span>' + Dex.escapeHTML(name) + '</a>';
+        var heldItemsHtml = this.renderHeldItemBadges(encounter);
+        return '<a class="location-pokemon-button" href="/pokemon/' + encounter.pokemon + '" data-target="push">' +
+            '<span class="picon" style="' + Dex.getPokemonIcon(encounter.pokemon) + '"></span>' +
+            '<span class="encounter-pokemon-name">' + Dex.escapeHTML(name) + '</span>' +
+            heldItemsHtml +
+            '</a>';
     }
 });
