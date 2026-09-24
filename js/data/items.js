f03204d92f0d17929ebea49775b48bacca8dea20
bluelobster06-oss <bluelobster06@gmail.com>
2026-09-24 23:27:16 +0200

    Changed item sprites, all Sebastian split locations

--- js/data/items.js
+++ js/data/items.js
@@ -682,7 +682,7 @@ exports.BattleItems = {
     },
     chillorb: {
         name: "Chill Orb",
-        icon: "https://bluelobster06-oss.github.io/Pokemon-Showdown-Dex/images/itemicons/static-orb.png",
+        icon: "https://bluelobster06-oss.github.io/Pokemon-Showdown-Dex/images/itemicons/chillorb-orb.png",
         fling: { basePower: 30, status: "frz" },
         onResidualOrder: 28,
         onResidualSubOrder: 3,
@@ -1055,7 +1055,7 @@ exports.BattleItems = {
     },
     dozeorb: {
         name: "Doze Orb",
-        icon: "https://bluelobster06-oss.github.io/Pokemon-Showdown-Dex/images/itemicons/static-orb.png",
+        icon: "https://bluelobster06-oss.github.io/Pokemon-Showdown-Dex/images/itemicons/doze-orb.png",
         fling: { basePower: 30, status: "slp" },
         onResidualOrder: 28,
         onResidualSubOrder: 3,
